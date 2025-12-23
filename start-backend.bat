6c46c63ff1cfa09a34d7ebf578996ccdbfb067a9
futurewave <futurewave@gmail.com>
2025-12-24 01:45:16 +0900

    윈도우즈 스크립트 수정

--- start-backend.bat
+++ start-backend.bat
@@ -23,8 +23,8 @@ if not exist "venv" (
     python -m venv venv
     call venv\Scripts\activate
     echo 의존성을 설치합니다...
-    pip install -r requirements.txt
     pip install pydantic[email]
+    pip install -r requirements.txt
 ) else (
     call venv\Scripts\activate
 )
